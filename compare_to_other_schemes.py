b3e1907cf5937e0aeb75ba86cf81bafc6d3d25e1
Greg D. Hill <greghill@cs.stanford.edu>
2017-03-23 21:51:39 +0000

    add draft of compare to other schemes

---
+++ compare_to_other_schemes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import re
+from sys import argv
+from subprocess import call
+
+emulators = [
+(       '2017-01-04T07-24-AWS-Brazil-1-to-Brazil-10-runs/',  '97.11',   '1',  '366',     '0',     '0'),
+(           '2017-01-02T03-54-India-to-AWS-India-10-runs/', '117.65',  '13',  '144',     '0',     '0'),
+(      '2016-12-30T21-38-China-ppp0-to-AWS-Korea-10-runs/',   '6.25', '152',  '362', '.0025',     '0'),
+('2016-12-30T22-50-AWS-Brazil-2-to-Colombia-ppp0-10-runs/',   '5.65',  '88', '3665', '.0026', '.0001'),
+(           '2017-01-03T21-30-Nepal-to-AWS-India-10-runs/',  '13.38',  '32',   '37',  '.003',     '0')
+]
+
+def koho_func(up_delay_threshold, up_delay_window_delta, down_delay_threshold, down_delay_window_delta, loss_window_delta):
+    thresholds_valid = float(down_delay_threshold - up_delay_threshold)
+    if thresholds_valid < 0.:
+        return {"score" : 999, "thresholds_valid" : thresholds_valid}
+
+    scores = []
+    #for run_id in range(1, len(emulators) + 1):
+    schemes = ["new_koho", "default_tcp", "vegas", "ledbat", "pcc", "verus", "scream", "sprout", "koho_cc"]
+    cmd = '../pantheon/test/test.py --uplink-trace %s --downlink-trace %s --prepend-mm-cmds "mm-delay %s mm-loss uplink %s mm-loss downlink %s" --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args=packets=%s" --extra-sender-args "%s" --run-id %d -t 30 new_koho' % (trace_dir, trace_dir, emulator[2], emulator[4], emulator[5], emulator[3], extra_sender_args, run_id)
+    #for scheme in schemes:
+        #emulator = emulators[run_id - 1]
+    emulator = emulators[0]
+    trace_dir = '../travis_extras/calibrated_emulators/%s%smbps.trace' % (emulator[0], emulator[1])
+    extra_sender_args = '%f %f %f %f %f' % (up_delay_threshold, up_delay_window_delta, down_delay_threshold, down_delay_window_delta, loss_window_delta)
+    cmd = '../pantheon/test/run.py --schemes "%s" --uplink-trace %s --downlink-trace %s --prepend-mm-cmds "mm-delay %s mm-loss uplink %s mm-loss downlink %s" --extra-mm-link-args "--uplink-queue=droptail --uplink-queue-args=packets=%s" --extra-sender-args "%s" --run-id %d -t 30 new_koho' % (' '.join(schemes), trace_dir, trace_dir, emulator[2], emulator[4], emulator[5], emulator[3], extra_sender_args, run_id)
+    print cmd
+    call(cmd, shell=True)
+
+if __name__ == '__main__':
+    koho_func(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
